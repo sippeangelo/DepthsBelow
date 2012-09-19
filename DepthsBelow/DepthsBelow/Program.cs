7b8a082e632eeaaae72e09acef22222f3f0b5735
imon <Simon@Simon-PC>
2012-09-19 13:11:00 +0200

    Changed the name of the "Game1" class to "Core"

--- DepthsBelow/DepthsBelow/Program.cs
+++ DepthsBelow/DepthsBelow/Program.cs
@@ -10,7 +10,7 @@ namespace DepthsBelow
         /// </summary>
         static void Main(string[] args)
         {
-            using (Game1 game = new Game1())
+            using (Core game = new Core())
             {
                 game.Run();
             }
